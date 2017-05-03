956c1f5e68907e5022cbd610d7058f29ef73abfc
gavinanthony <iam@gavinanthony.com>
2017-05-02 22:19:24 -0500

    working on react router transitions

---
+++ javascripts/components/RouteTransitionGroup.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Route } from 'react-router-dom'
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
+
+const RouteTransitionGroup = ({ children }) => (
+  <Route render={({ location }) => (
+    <CSSTransitionGroup
+      transitionName='example'
+      transitionAppear={true}
+      transitionAppearTimeout={500}
+      transitionEnterTimeout={500}
+      transitionLeaveTimeout={0}
+    >
+      <div key={location.key}>
+        {children}
+      </div>
+    </CSSTransitionGroup>
+  )} />
+)
+
+RouteTransitionGroup.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default RouteTransitionGroup
--- javascripts/index.js
+++ javascripts/index.js
@@ -3,10 +3,12 @@ import { render } from 'react-dom'
 import {
   BrowserRouter as Router,
   Route,
+  Switch,
 } from 'react-router-dom'
 
 import styles from '../stylesheets/app.css'
 
+import RouteTransitionGroup from './components/RouteTransitionGroup'
 import Nav from './components/Nav'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -17,10 +19,12 @@ function Root() {
     <Router>
       <div>
         <Nav />
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/work/:clientName" component={Home} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/work/:clientName" component={Home} />
+        </Switch>
       </div>
     </Router>
   )
