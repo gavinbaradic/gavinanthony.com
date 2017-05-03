b294e95796301a851b3d2bb09d50470711ebbfc4
gavinanthony <iam@gavinanthony.com>
2017-05-02 19:59:13 -0500

    contact form & lost grid

--- javascripts/pages/Contact.js
+++ javascripts/pages/Contact.js
@@ -2,6 +2,30 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <section className="contact container small">
+      <div className="col-1-of-1">
+        <h1>Let's Talk</h1>
+        <h2>
+          Reach out for a project or just to say hello! My personal email is <a href="mailto:iam@gavinanthony.com">iam@gavinanthony.com</a>
+        </h2>
+      </div>
+      <form className="contact-form grid-row">
+        <div className="formrow col-1-of-2">
+          <label htmlFor="name">Full Name</label>
+          <input id="name" placeholder="Alex Smith" />
+        </div>
+        <div className="formrow col-1-of-2">
+          <label>Email</label>
+          <input placeholder="alex.smith@gmail.com" />
+        </div>
+        <div className="formrow col-1-of-1">
+          <label>Message</label>
+          <textarea placeholder="Hello..." />
+        </div>
+        <div className="formrow col-1-of-1">
+          <button>Send Message</button>
+        </div>
+      </form>
+    </section>
   )
 }
