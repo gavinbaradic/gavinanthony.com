9c979658b1d34c0bca17a8dadd1be00a35562062
gavinanthony <iam@gavinanthony.com>
2017-05-03 11:01:50 -0500

    contact form submission

--- javascripts/pages/Contact.js
+++ javascripts/pages/Contact.js
@@ -1,31 +1,73 @@
 import React from 'react'
 
-export default function Contact() {
-  return (
-    <section className="contact container small">
-      <div className="col-1-of-1">
-        <h1>Let's Talk</h1>
-        <h2>
-          Reach out for a project or just to say hello! My personal email is <a href="mailto:iam@gavinanthony.com">iam@gavinanthony.com</a>
-        </h2>
-      </div>
-      <form className="contact-form grid-row">
-        <div className="formrow col-1-of-2">
-          <label htmlFor="name">Full Name</label>
-          <input id="name" placeholder="Alex Smith" />
+import { formSubmission } from '../utils/queries'
+import graphql from '../utils/graphql'
+
+export default class Contact extends React.Component {
+  state = {
+    submitted: false,
+    isSubmitting: false,
+    name: '',
+    email: '',
+    message: '',
+  }
+
+  handleInputChange = ({ target }) => {
+    const { value, id } = target
+
+    this.setState({ [id]: value })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.setState({ isSubmitting: true })
+
+    const { name, email, message } = this.state
+    const variables = { name, email, message }
+
+    graphql({ query: formSubmission, variables })
+      .then(res => this.setState({ isSubmitting: false, submitted: true }))
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <section className="contact container small">
+        <div className="col-1-of-1">
+          <h1>Let's Talk</h1>
+          <h2>
+            Reach out for a project or just to say hello! My personal email is <a href="mailto:iam@gavinanthony.com">iam@gavinanthony.com</a>
+          </h2>
         </div>
-        <div className="formrow col-1-of-2">
-          <label>Email</label>
-          <input placeholder="alex.smith@gmail.com" />
-        </div>
-        <div className="formrow col-1-of-1">
-          <label>Message</label>
-          <textarea placeholder="Hello..." />
-        </div>
-        <div className="formrow col-1-of-1">
-          <button>Send Message</button>
-        </div>
-      </form>
-    </section>
-  )
+        <form className="contact-form grid-row">
+          <div className="formrow col-1-of-2">
+            <label htmlFor="name">Full Name</label>
+            <input
+              id="name"
+              placeholder="Alex Smith"
+              onChange={this.handleInputChange} />
+          </div>
+          <div className="formrow col-1-of-2">
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              placeholder="alex.smith@gmail.com"
+              onChange={this.handleInputChange} />
+          </div>
+          <div className="formrow col-1-of-1">
+            <label htmlFor="message">Message</label>
+            <textarea
+              id="message"
+              placeholder="Hello..."
+              onChange={this.handleInputChange} />
+          </div>
+          <div className="formrow col-1-of-1">
+            <button onClick={this.handleSubmit}>
+              {this.state.submitted ? 'Thank you!' : 'Send Message' }
+            </button>
+          </div>
+        </form>
+      </section>
+    )
+  }
 }
--- javascripts/utils/queries.js
+++ javascripts/utils/queries.js
@@ -6,3 +6,15 @@ export const getClients = `
     }
   }
 `
+
+export const formSubmission = `
+  mutation createContactForm($email:String!, $name:String!, $message:String!) {
+    createContactForm(
+      email: $email,
+      name: $name,
+      message: $message
+    ) {
+      id
+    }
+  }
+`
