65d466401c312182b35381d747364d85b8ce384f
gavinanthony <iam@gavinanthony.com>
2017-05-05 10:19:18 -0500

    about page progress

--- javascripts/pages/About.js
+++ javascripts/pages/About.js
@@ -8,6 +8,13 @@ export default class About extends React.Component {
   render() {
     return (
       <div>
+        <section className="container photo">
+          <div className="col-8-of-12 portrait" />
+          <div className="col-4-of-12 pull-4">
+            <h1>About Me</h1>
+            <h2>Designer, Developer</h2>
+          </div>
+        </section>
         <section className="container profile">
           <div className="col-5-of-12">
             Profile
