38a609dea912550ffb05eda43429f4e1a8972b67
gavinanthony <iam@gavinanthony.com>
2017-05-07 21:02:47 -0500

    profile text

--- javascripts/pages/About.js
+++ javascripts/pages/About.js
@@ -20,7 +20,12 @@ export default class About extends React.Component {
             Profile
           </div>
           <div className="col-7-of-12">
-            Lorem
+            <p>
+              Polaroid banh mi biodiesel gastropub, readymade echo park distillery dreamcatcher narwhal everyday carry. Before they sold out tumblr gochujang ramps gentrify, trust fund polaroid hoodie post-ironic selvage. Deep v venmo food truck fashion axe, wolf af pug cray art party vice poutine chambray scenester wayfarers. Twee try-hard brooklyn, lo-fi vexillologist williamsburg blue bottle salvia wayfarers tote bag.
+            </p>
+            <p>
+              Flexitarian aesthetic bicycle rights readymade intelligentsia man bun. Tofu roof party polaroid, +1 subway tile four dollar toast kinfolk coloring book health goth. Bitters la croix deep v lumbersexual polaroid.
+            </p>
           </div>
         </section>
         <section className="container profile">
