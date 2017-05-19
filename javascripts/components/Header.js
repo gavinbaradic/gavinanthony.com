0bde24ea64bbfa43a9cfeb0bd29838e0165cfef2
gavinanthony <iam@gavinanthony.com>
2017-05-19 09:29:42 -0500

    header tweaks

--- javascripts/components/Header.js
+++ javascripts/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
     <header className="container header">
       <h1>Product designer & developer from Milwaukee, WI.</h1>
       <h2>
-        I work with startups and businesses to create great experiences on web & mobile applications. <a href="/">See my work</a>
+        I work with startups and businesses to create great experiences on web & mobile applications.
+        {' '}
+        <Link to="/contact" className="btn dark">Get in touch</Link>
       </h2>
     </header>
   )
