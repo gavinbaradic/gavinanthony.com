433a792b40a50d2e485a166f05763d51df9b410a
gavinanthony <iam@gavinanthony.com>
2017-05-23 22:36:12 -0500

    blockquote author

--- javascripts/clients/Taplet.js
+++ javascripts/clients/Taplet.js
@@ -15,6 +15,7 @@ const Taplet = () => (
       <div className="client-quote">
         <blockquote>
           “The iPhone has offered the ability to take photos at the same time as you record video, but capturing photos from video that has already been captured isn’t quite so straightforward. Taplet aims to solve that.”
+          <span>— Techcrunch</span>
         </blockquote>
       </div>
     </section>
