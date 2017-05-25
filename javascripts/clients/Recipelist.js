65db4db6f92554f4c15969ebd504e098644339d5
gavinanthony <iam@gavinanthony.com>
2017-05-25 11:21:59 -0500

    recipelist case study progress

--- javascripts/clients/Recipelist.js
+++ javascripts/clients/Recipelist.js
@@ -1,7 +1,46 @@
 import React from 'react'
 
 const Recipelist = () => (
-  <div><section className="container full">Hello</section></div>
+  <div>
+    <section className="container full">
+      <div>
+        <img
+          src={require('img/recipelist/macbook.jpg')}
+          alt="Recipelist For Web"
+        />
+      </div>
+    </section>
+    <section className="container">
+      <div className="client-quote">
+        <blockquote>
+          Vaporware church-key hell of, synth hoodie banjo sartorial authentic forage meggings aesthetic hot chicken. Drinking vinegar kickstarter try-hard unicorn scenester
+        </blockquote>
+      </div>
+    </section>
+    <section className="container">
+      <div>
+        <img
+          src={require('img/recipelist/iphones.png')}
+          alt="Recipelist Phones"
+        />
+      </div>
+    </section>
+    <section className="container">
+      <div className="client-quote">
+        <blockquote>
+          Vaporware church-key hell of, synth hoodie banjo sartorial authentic forage meggings aesthetic hot chicken. Drinking vinegar kickstarter try-hard unicorn scenester
+        </blockquote>
+      </div>
+    </section>
+    <section className="container full">
+      <div>
+        <img
+          src={require('img/recipelist/branding.svg')}
+          alt="Recipelist Branding"
+        />
+      </div>
+    </section>
+  </div>
 )
 
 export default Recipelist
