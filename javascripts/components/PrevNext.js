dd2427cd1bb643d5b5fb478723653cff6185a37a
gavinanthony <iam@gavinanthony.com>
2017-06-01 10:14:54 -0500

    move between clients via arrow keys

--- javascripts/components/PrevNext.js
+++ javascripts/components/PrevNext.js
@@ -18,13 +18,29 @@ const getNextClient = ({ allClients, clientId }) => {
   return allClients[clientId + 1]
 }
 
-const PrevNext = props => (
-  <div>
-    <section className="container prevnext">
-      <Card client={getPrevClient(props)} direction="Previous" />
-      <Card client={getNextClient(props)} direction="Next" />
-    </section>
-  </div>
-)
+const PrevNext = (props) => {
+  const prevClient = getPrevClient(props)
+  const nextClient = getNextClient(props)
+
+  document.onkeydown = (e) => {
+    switch (e.keyCode) {
+      case 37:
+        window.location = `/work/${prevClient.slug}`
+        break
+      case 39:
+        window.location = `/work/${nextClient.slug}`
+        break
+    }
+  }
+
+  return (
+    <div>
+      <section className="container prevnext">
+        <Card client={prevClient} direction="Previous" />
+        <Card client={nextClient} direction="Next" />
+      </section>
+    </div>
+  )
+}
 
 export default PrevNext
