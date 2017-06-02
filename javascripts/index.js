aaff6eb200f6dfeba97794777a1a804ebdb8bece
gavinanthony <iam@gavinanthony.com>
2017-06-02 11:51:08 -0500

    google analytics

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import ReactGA from 'react-ga'
 
 import styles from '../stylesheets/app.css'
 
@@ -11,16 +12,25 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import Client from './pages/Client'
 
+ReactGA.initialize('UA-83701510-1')
+
+const GoogleAnalytics = () => {
+  ReactGA.set({ page: window.location.pathname })
+  ReactGA.pageview(window.location.pathname)
+  return null
+}
+
 const ScrollToTop = () => {
   window.scrollTo(0, 0)
   return null
 }
 
 const Root = () => (
-  <Router>
+  <Router history={history}>
     <div>
       <Nav />
       <Route component={ScrollToTop} />
+      <GoogleAnalytics />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
