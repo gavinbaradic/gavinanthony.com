53de53bfaa804a83e49b241f799b359e5443aeab
gavinanthony <iam@gavinanthony.com>
2017-06-03 14:37:40 -0500

    blip

--- javascripts/clients/Blip.js
+++ javascripts/clients/Blip.js
@@ -7,6 +7,18 @@ const Blip = () => (
         <img src={require('img/blip/phone-in-hand.jpg')} alt="Blip Feature" />
       </div>
     </section>
+    <section className="container">
+      <div className="client-quote">
+        <blockquote>
+          Discover fun things to do and great ways to meet new friends on your campus right now. Send a private blip and invite your friends to do something, meet up with people in a crowded area, or show a friend that you’re on the way.
+        </blockquote>
+      </div>
+    </section>
+    <section className="container">
+      <div>
+        <img src={require('img/blip/iphones.png')} alt="Blip Phones" />
+      </div>
+    </section>
   </div>
 )
 
